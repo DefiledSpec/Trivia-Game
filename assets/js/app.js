b280bb5e48ce42b514afce88b094c37cfab7feb8
DefiledSpec <defiledspec@gmail.com>
2018-06-13 12:43:01 -0500

    added timer to game, difficulty option, new game and continue buttons

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,52 @@
-let letters = ['A.', 'B.', 'C.', 'D.', 'E.'];
+let letters = ['A', 'B', 'C', 'D', 'E'];
 let currentQuestion;
 let correct = null;
-let score;
-let runningScore = 0;
+let score, runningScore = 0;
 let questionsAnswered;
+let questionTimer;
+let quizContainer = $('.quizContainer');
+let counterId;
+let counter;
+let triviaCompleted, questionQty = 10, difficulty;
+let queryURL;
 
 function start() {
+    quizContainer.empty();
+    if(runningScore >= 1) {
+        quizContainer.append($('<button>').addClass('startGame').text('Continue'));
+    }
+    $('.counter').empty();
+    quizContainer.append($('<button>').addClass('startOver').text('New Game'))
+    addDifficulty();
     currentQuestion = 0;
     questionsAnswered = 0;
     score = 0;
-    getQuestion();
-    
+    console.log($('.difficulty').val())
+   
+}
+function addDifficulty(){
+    var myOptions = {
+        easy : 'Easy',
+        medium : 'Medium',
+        hard : 'Hard',
+        any : 'Any'
+
+    };
+    let selectArea = $('<select>').addClass('difficulty');
+    quizContainer.append(selectArea);
+    $.each(myOptions, function(val, text) {
+        selectArea.append($('<option>').val(val).html(text));
+    });
+
 }
 
 function getQuestion() {
-    let queryURL = `https://opentdb.com/api.php?amount=1`;
-    if(questionsAnswered >= 10){
+    if(!difficulty || difficulty === 'any') {
+        queryUrl = `https://opentdb.com/api.php?amount=1`
+    } else {
+        queryURL = `https://opentdb.com/api.php?amount=1&difficulty=${difficulty}`;
+    }
+    if(questionsAnswered >= questionQty){
         start();
         return
     }
@@ -25,21 +56,39 @@ function getQuestion() {
             showQuestion(response.results[0]);
         });
 }
+function runCounter() {
+    counter = 10;
+    let counterDiv = $('.counter').html(`Time Left: ${counter}`);
+    counterId = setInterval(function() {
+        counter--;
+        console.log(`Time Left: ${counter}`)
+        counterDiv.html(`Time Left: ${counter}`);
+    }, 1000);
+}
 
 function showQuestion(res) {
+    clearTimeout(questionTimer)
+    runCounter()
+
+    questionTimer = setTimeout(function() {
+        clearInterval(counterId);
+        checkAnswer();
+    }, 10000);
     currentQuestion++;
-    $('#currentQuestion').text(`${currentQuestion}/10`)
-    console.log(res)
+    $('#currentQuestion').text(`Current Question: ${currentQuestion} / 10`)
+    // console.log(res)
     correct = res.correct_answer;
     let unshuffled = [...res.incorrect_answers, res.correct_answer];
     let answers = shuffle(unshuffled);
     let answerList = $('<ul>')
-    console.log(answers)
-    let quizContainer = $('.quizContainer').empty().text(`Score: ${runningScore} ${score}`)
+    // console.log(answers)
+    quizContainer.empty().text(`Score: ${runningScore}`)
         .append($('<p>').addClass('question').html(res.question))
         .append(answerList)
     for(let i = 0; i < answers.length; i++) {
-        answerList.append($('<li>').addClass('answers').attr('data-answer', answers[i]).html(`${letters[i]} ${answers[i]}`));
+        answerList.append($('<li>')
+            .addClass('answers').attr('data-answer', answers[i])
+            .html(`${letters[i]}. ${answers[i]}`));
     }
 }
 function shuffle(array) {
@@ -50,20 +99,36 @@ function shuffle(array) {
     return array;
 }
 function checkAnswer(answer) {
+    clearTimeout(questionTimer);
+    clearInterval(counterId);
+    
+    if(!answer) {
+        console.log('Times up!');
+    }
     if(answer === correct) {
         score++;
         runningScore++;
+        $('.counter').empty();
+        quizContainer.html('Correct!' + answer);
+        
+    }else{
+        quizContainer.html('Wrong! The answer is ' + correct);
     }
     questionsAnswered++;
+    setTimeout(getQuestion, 2500);
+}
+$(document).on('click', '.startGame', function() {
     getQuestion();
-}
-
-$(document).on('click', '.answers', handleAnswer)
-function handleAnswer() {
-    let answ = $(this).attr('data-answer');
-    checkAnswer(answ) 
-}
+    difficulty = $('.difficulty').val();
+    console.log(difficulty)
+});
+$(document).on('click', '.startOver', function() {
+    difficulty = $('.difficulty').val();
+    runningScore = 0;
+    getQuestion();
+})
+$(document).on('click', '.answers', function() {
+    checkAnswer($(this).attr('data-answer')) 
+});
 
 start();
-
-
